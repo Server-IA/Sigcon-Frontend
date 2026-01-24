0175c447a4cbd9452d934dd226a655f24dd2e446
julian-andres-silva <juliansilvamorales28@gmail.com>
2026-01-23 19:51:24 -0500

    login

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,19 @@
 import { Route } from 'react-router-dom';
 import MainTemplate from '../components/templates/MainTemplate';
+import AuthTemplate from '../components/templates/AuthTemplate';
 
-
+import Login from "../pages/Login/LoginPage";
 import Home from '../pages/home/index'
 
 export const RenderRoutes = () => {
     return (
         <>
+        {/* Auth Routes */}
+        <Route element={<AuthTemplate />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+
             <Route element={<MainTemplate />}>
                 <Route path="dashboard">
                     <Route index element={<Home />} />
