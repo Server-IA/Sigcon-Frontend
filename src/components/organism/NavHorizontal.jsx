d25f50923dceab9e0d3592f6dbf952dca6f88980
WilliamsBD8 <wsbonilladiaz@gmail.com>
2026-02-09 23:00:30 -0500

    Refactor asset paths and environment configuration
    - Updated index.html to use dynamic base URL for asset paths.
    - Modified Dockerfile to remove unused build arguments for VITE_BASE_PATH and VITE_APP_ENV.
    - Adjusted Vite configuration to set a static base path.
    - Set BrowserRouter basename in App component for routing consistency.

--- src/components/organism/NavHorizontal.jsx
+++ src/components/organism/NavHorizontal.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
 
 const NavHorizontal = ({modules}) => {
 
--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      jquery: 'jquery'
+export default defineConfig(({ mode }) => {
+
+  return {
+    base: '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        jquery: 'jquery'
+      }
+    },
+    optimizeDeps: {
+      include: [
+        'jquery',
+        'datatables.net',
+        'datatables.net-bs5',
+        'datatables.net-buttons',
+        'typeahead.js'
+      ]
     }
-  },
-  optimizeDeps: {
-    include: [
-      'jquery',
-      'datatables.net',
-      'datatables.net-bs5',
-      'datatables.net-buttons',
-      'typeahead.js'
-    ]
   }
-})
+});
+
